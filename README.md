906cc08f22be55d7d1784c98563a5ffb0e1bdd03
Diwamoto <daiki.work.0118@gmail.com>
2022-02-07 01:21:24 +0900

    [add] front-standard with webpack

---
+++ README.md
@@ -0,0 +1,15 @@
+# Front standard with webpack
+
+This is a development environment with a simple html/sass/js configuration using webpack.
+You can use browser-sync for fast development.
+It can be deployed immediately using vercel.
+
+## dev server
+```
+yarn dev
+```
+
+## build for production
+```
+yarn build
+```
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const HTML_PATH = path.join(__dirname, './src/html');
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
+
+const config = {
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: HTML_PATH + "/index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(ico|eot|svg|ttf|woff|woff2|png|jpe?g|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
