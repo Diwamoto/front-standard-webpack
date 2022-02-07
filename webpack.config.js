a8a440bef6bd169b1efbf86f58c4e60f5052674d
Diwamoto <daiki.work.0118@gmail.com>
2022-02-08 01:53:22 +0900

    [update] implement of multi html files

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,31 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const isProduction = process.env.NODE_ENV == "production";
+
 const HTML_PATH = path.join(__dirname, './src/html');
 
-const isProduction = process.env.NODE_ENV == "production";
+const routes = [
+    {   
+        //index
+        template: HTML_PATH + "/index.html", 
+        file: "index.html"
+    }, 
+    {
+        //test
+        template: HTML_PATH + "/test.html", 
+        file: "test.html"
+    }
+];
+
+const htmlPlugins = () => {
+  return routes.map(entry => {
+      return new HtmlWebpackPlugin({
+          template: entry.template,
+          filename: entry.file,
+      });
+  })
+};
 
 const stylesHandler = isProduction
   ? MiniCssExtractPlugin.loader
@@ -21,14 +43,7 @@ const config = {
     open: true,
     host: "localhost",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: HTML_PATH + "/index.html",
-    }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
+  plugins: htmlPlugins(),
   module: {
     rules: [
       {
